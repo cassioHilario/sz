0a9b88c2708f4a613e65fb8ab0c1d7fdfca2efb4
Cássio Antônio Ferreira Hilário <cacaafh2000@gmail.com>
2024-04-23 14:24:07 -0300

    Update melhoras.py

--- melhoras.py
+++ melhoras.py
@@ -6,12 +6,12 @@ class Nenem:
 if __name__ == "__main__":
 
   def desejar_melhoras(nenem):
-      if nenem.saude == "doentinha":
+      if nenem.saude == 1:
           return "Fica bem logo, meu nenem. Tô aqui pro que precisar, apenas a uma ligação de distância"
       else:
           return "Espero que você esteja melhor. Me liga que já to indo te ver"
   
-  saude_nenem = "doentinha"
+  saude_nenem = int(input("Como está se sentido hoje? \n Digite: \n 1 - para doentinha \n 2 - para \"tô melhor\"\n"))
   
   meu_nenem = Nenem(saude_nenem)
   
